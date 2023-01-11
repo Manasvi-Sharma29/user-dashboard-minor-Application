e43a2a984e06ae17b55a45f297ecad58d984d19a
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2023-01-12 02:27:56 +0530

    boiler plate

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const route = require('./Routes/route')
+
+
+const app = express()
+app.use(express.json())
+
+
+mongoose.connect(
+    "mongodb+srv://Manasvi29:bharat2909@cluster0.r7a9dpa.mongodb.net/user-Dashboard-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+).then(()=>console.log("MongoDB Connected"))
+.catch((err)=>console.log(err))
+
+
+app.use('/',route)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express app running on port " + (process.env.PORT || 3000))
+})
+
+
